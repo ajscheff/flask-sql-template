8ef9f75cfd6ff1c3907ea7ca7f4b870bb6a9fa56
Andy Scheff <andyscheff@gmail.com>
2020-04-25 14:59:17 -0700

    basic stencil

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost:5432'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+
+    def __repr__(self):
+        return self.name
+
+@app.route('/')
+def home():
+    results = User.query.all()
+    return str(results)
+
+def seed_data():
+    db.session.add(User(name='George'))
+    db.session.commit()
